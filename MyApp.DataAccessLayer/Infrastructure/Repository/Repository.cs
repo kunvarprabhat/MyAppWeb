3e3f617bed903ba68a505b3952a4861d969760ea
kunvar prabhat <kunvarprabhat44@gmail.com>
2023-05-03 07:09:32 +0530

    Add project files.

---
+++ MyApp.DataAccessLayer/Infrastructure/Repository/Repository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApp.DataAccessLayer.Infrastructure.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.DataAccessLayer.Infrastructure.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private DbSet<T> _dbSet;
+
+        public  Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        void IRepository<T>.Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        void IRepository<T>.Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        void IRepository<T>.DeleteRange(IEnumerable<T> entity)
+        {
+            _dbSet.RemoveRange(entity);
+        }
+
+        IEnumerable<T> IRepository<T>.GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+       public T GetT(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate).FirstOrDefault();
+        }
+    }
+}
---
+++ MyAppWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using MyApp.Models;
+using Microsoft.EntityFrameworkCore;
+using MyApp.DataAccessLayer;
+using MyApp.DataAccessLayer.Infrastructure.IRepository;
+
+namespace MyAppWeb.Areas.Admin.Controllers
+{
+      //[Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private IUnitOfWork _unitofWork;
+           
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitofWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _unitofWork.Category.GetAll();
+            return View(categories);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            // ServerSideValidation
+            if (ModelState.IsValid)
+            {
+                _unitofWork.Category.Add(category);
+                _unitofWork.Save();
+                //tempdata- single request mai work karta hai
+                TempData["success"] = "Category Created Done!";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _unitofWork.Category.GetT(x => x.id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            // ServerSideValidation
+            if (ModelState.IsValid)
+            {
+                _unitofWork.Category.Update(category);
+                _unitofWork.Save();
+                TempData["success"] = "Category Update Done!";
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+
+        }
+
+
+
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _unitofWork.Category.GetT(x => x.id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteData(int? id)
+        {
+            // Delete 
+            var category = _unitofWork.Category.GetT(x => x.id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _unitofWork.Category.Delete(category);
+            _unitofWork.Save();
+            TempData["success"] = "Category Deleted Done!";
+            return RedirectToAction("Index");
+
+        }
+
+
+    }
+}
